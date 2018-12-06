d1a9242078129cf2c17c8bce52b1641b429f161c
jzohdi <jzohdi@terpmail.umd.edu>
2018-12-06 17:56:15 -0500

    initial commit

---
+++ motion.js
@@ -0,0 +1,105 @@
+function init() {
+  var canvas = document.getElementById("map-canvas2");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  var cb = canvas.getContext("2d");
+
+  function randomIntRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  var mouse = {
+    x: undefined,
+    y: undefined
+  };
+
+  window.addEventListener("mousemove", function() {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+  });
+
+  function Particle(x, y, radius, color) {
+    this.centerx = x;
+    this.centery = y;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.radians = Math.random() * Math.PI * 2;
+    this.velocity = 0.008;
+    this.path = randomIntRange(150, 400);
+
+    this.draw = function() {
+      cb.beginPath();
+      cb.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      cb.fillStyle = this.color;
+      cb.fill();
+      cb.closePath();
+    };
+
+    this.update = function() {
+      this.radians += this.velocity;
+      this.x = this.centerx + Math.cos(this.radians) * this.path;
+      this.y = this.centery + Math.sin(this.radians) * this.path;
+
+      if (
+        mouse.x >= 20 &&
+        mouse.x <= innerWidth - 20 &&
+        mouse.y >= 20 &&
+        mouse.y <= innerHeight - 20
+      ) {
+        if (this.centerx >= mouse.x + 5) {
+          this.centerx -= 2;
+        }
+        if (this.centerx <= mouse.x - 5) {
+          this.centerx += 2;
+        }
+        if (this.centery >= mouse.y + 5) {
+          this.centery -= 2;
+        }
+        if (this.centery <= mouse.y - 5) {
+          this.centery += 2;
+        }
+        // this.centerx = mouse.x;
+        // this.centery = mouse.y;
+      } else {
+        if (this.centerx >= innerWidth / 2 + 2) {
+          this.centerx /= 1.01;
+        }
+        if (this.centerx <= innerWidth / 2 - 2) {
+          this.centerx *= 1.01;
+        }
+        // this.centerx = innerWidth / 2;
+        if (this.centery >= innerHeight / 2 + 2) {
+          this.centery /= 1.01;
+        }
+        if (this.centery <= innerHeight / 2 - 2) {
+          this.centery *= 1.01;
+        }
+      }
+      this.draw();
+    };
+  }
+  var particles = [];
+
+  function init1() {
+    for (var i = 0; i < 200; i++) {
+      particles.push(new Particle(innerWidth / 2, innerHeight / 2, 5, "blue"));
+    }
+  }
+
+  init1();
+
+  function animate() {
+    requestAnimationFrame(animate);
+    // cb.clearRect(0, 0, innerWidth, innerHeight);
+    cb.fillStyle = "rgba(255, 255, 255, 0.05)";
+    cb.fillRect(0, 0, innerWidth, innerHeight);
+    for (var n = 0; n < particles.length; n++) {
+      particles[n].update();
+    }
+  }
+
+  animate();
+}
---
+++ particle.js
@@ -0,0 +1,150 @@
+function initO() {
+  var canvas = document.getElementById("map-canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  var centerX = canvas.width / 2;
+  var centerY = canvas.height / 2;
+
+  var cb = canvas.getContext("2d");
+
+  // $("#add-styles").append(
+  //   "#right{transform:translate(" +
+  //     -20 +
+  //     "px," +
+  //     (-centerY).toString() +
+  //     "px);float:right;}#left{transform:translate(" +
+  //     20 +
+  //     "px," +
+  //     (-centerY).toString() +
+  //     "px); float:left;}"
+  // );
+  function randomIntRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  var canvasOptions = [
+    "<canvas id='map-canvas'></canvas>",
+    "<canvas id='map-canvas2'></canvas>"
+  ];
+  var currentCanvas = 0;
+  // var scriptsOptions = [initO(), init()];
+  document.getElementById("right").addEventListener("click", function() {
+    setNewCanvas(1);
+  });
+
+  document.getElementById("left").addEventListener("click", function() {
+    setNewCanvas(-1);
+  });
+
+  function setNewCanvas(num) {
+    let index = Math.abs(currentCanvas + num) % canvasOptions.length;
+    currentCanvas = index;
+
+    $("#new-canvas").empty();
+    $("#new-canvas").append(canvasOptions[index]);
+
+    $("#scripts").empty();
+
+    if (index == 1) {
+      $("#scripts").append(init());
+    }
+    if (index == 0) {
+      $("#scripts").append(initO());
+    }
+  }
+  cb.fillStyle = "black";
+  cb.fillRect(0, 0, innerWidth, innerHeight);
+
+  function Proton(offsetX, offsetY, radius, color) {
+    this.x = centerX + offsetX;
+    this.y = centerY + offsetY;
+    this.radius = radius;
+    this.color = color;
+
+    this.draw = function() {
+      cb.beginPath();
+      cb.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      cb.fillStyle = this.color;
+      cb.fill();
+      cb.closePath();
+    };
+  }
+
+  function Electrons(offsetX, offsetY, radius, color) {
+    this.centerx = centerX;
+    this.centery = centerY;
+    this.x = centerX + offsetX;
+    this.y = centerY + offsetY;
+    this.radius = radius;
+    this.color = color;
+    this.radians = Math.random() * Math.PI * 2;
+    this.velocity = 0.015;
+    this.path = randomIntRange(150, 400);
+    this.angle1 =
+      (Math.random() * (1.0 - 0.3) + 0.3).toFixed(4) *
+      (Math.floor(Math.random() * 2) == 1 ? 1 : -1);
+    this.angle2 =
+      (Math.random() * (1.0 - 0.3) + 0.3).toFixed(4) *
+      (Math.floor(Math.random() * 2) == 1 ? 1 : -1);
+
+    this.draw = function() {
+      cb.beginPath();
+      cb.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      cb.fillStyle = this.color;
+      cb.fill();
+      cb.closePath();
+    };
+
+    this.update = function() {
+      this.radians += this.velocity;
+
+      this.x = this.centerx + Math.cos(this.radians) * this.path * this.angle1;
+      this.y = this.centery + Math.sin(this.radians) * this.path * this.angle2;
+
+      this.draw();
+    };
+  }
+
+  var neuclius = [
+    new Proton(7, 7, 10, "grey"),
+    new Proton(-7, -7, 10, "grey"),
+    new Proton(7, -7, 10, "white"),
+    new Proton(-7, 7, 10, "white"),
+    new Proton(0, 0, 10, "grey")
+  ];
+
+  var electron_arr = [];
+  for (let x = 0; x < 100; x++) {
+    electron_arr.push(
+      new Electrons(
+        randomIntRange(50, 100),
+        randomIntRange(50, 100),
+        3,
+        "white"
+      )
+    );
+  }
+
+  document.getElementById("map-canvas").addEventListener("click", function() {
+    electron_arr.forEach(function(ele) {
+      ele.velocity *= -1;
+    });
+  });
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    cb.fillStyle = "rgba(0, 0, 0, 0.05)";
+    cb.fillRect(0, 0, innerWidth, innerHeight);
+
+    neuclius.forEach(function(element) {
+      element.draw();
+    });
+
+    electron_arr.forEach(function(element) {
+      element.update();
+    });
+  }
+
+  animate();
+}
