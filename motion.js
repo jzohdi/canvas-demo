709fcea457e8a8ba1298c89f3fc2c59c85e73cfb
jzohdi <jzohdi@terpmail.umd.edu>
2018-12-06 18:40:31 -0500

    sizing motion js

--- motion.js
+++ motion.js
@@ -37,7 +37,10 @@ function init() {
     this.color = color;
     this.radians = Math.random() * Math.PI * 2;
     this.velocity = 0.008;
-    this.path = randomIntRange(canvas.width / 10, canvas.width / 4);
+    this.path = randomIntRange(
+      canvas.width / 10,
+      Math.min(canvas.width, canvas.height)
+    );
     this.speed = 3;
 
     this.draw = function() {
